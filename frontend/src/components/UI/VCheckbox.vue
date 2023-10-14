9b867c11afa63212bab043a9d0337f1ebac28594
Anna Gubskaya <gubskaya.ann@gmail.com>
2023-10-14 13:52:00 +0300

    update checkbox

--- frontend/src/components/UI/VCheckbox.vue
+++ frontend/src/components/UI/VCheckbox.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="chbx">
-    <div class="chbx__checkbox">
-      <input type="checkbox" class="chbx__radio chbx__radio-second" :id="props.name" :name="props.name" />
-      <label :for="props.name" v-html="props.text"></label>
-    </div>
+  <div class="chbx__checkbox">
+    <label class="chbx__checkbox-label" :for="props.name">
+      <input type="checkbox" class="chbx__checkbox-input" :id="props.name" :name="props.name" />
+    </label>
+    <p class="chbx__checkbox-text">{{ props.text }}</p>
   </div>
 </template>
 
@@ -30,95 +30,89 @@ const props = defineProps({
 </script>
 
 <style lang="scss" scoped>
-.chbx {
-  &__radio {
-    vertical-align: top;
-    width: 1.6rem;
-    height: 1.6rem;
-    margin: 0 0.3rem 0 0;
+.chbx__checkbox {
+  position: relative;
+  padding-left: 4rem;
+  min-height: 2rem;
+  width: 100%;
+  &-input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    &::before {
+      content: '';
+      width: 1.6rem;
+      height: 1.6rem;
+      border-radius: $base-border-radius;
+      position: absolute;
+      background-color: $grey;
+      top: 0;
+      bottom: 0;
+      margin-top: auto;
+      margin-bottom: auto;
+      left: 0;
+      border: 3px solid $grey-clear;
+      box-shadow: $base-shadow;
+      cursor: pointer;
+    }
+    &::after {
+      content: '';
+      width: 1.2rem;
+      height: 1.2rem;
+      border-radius: $base-border-radius;
+      position: absolute;
+      background: url('@/assets/icons/checkmark.svg') no-repeat;
+      background-size: contain;
+      top: 0;
+      bottom: 0;
+      margin-top: auto;
+      margin-bottom: auto;
+      left: -0.4rem;
+      visibility: hidden;
+      opacity: 0;
+      transition: all 0.2s ease-in;
+      cursor: pointer;
+    }
+    &:checked {
+      &::after {
+        content: '';
+        width: 1.6rem;
+        height: 1.6rem;
+        border-radius: $base-border-radius;
+        position: absolute;
+        background: url('@/assets/icons/checkmark.svg') no-repeat;
+        background-size: contain;
+        top: 0;
+        bottom: 0;
+        margin-top: auto;
+        margin-bottom: auto;
+        left: 3px;
+        visibility: visible;
+        opacity: 1;
+        transition: all 0.1s ease-out;
+        cursor: pointer;
+      }
+    }
   }
-  &__radio + label {
-    cursor: pointer;
-  }
-  &__radio:not(checked) {
+  &-label {
     position: absolute;
-    opacity: 0;
-    border-radius: 0.3rem;
-  }
-  &__radio:not(checked) + label {
-    position: relative;
-    padding: 0 0 0 3.6rem;
-    display: block;
-  }
-  &__radio-first:not(checked) + label:before {
-    top: 3.6rem;
-  }
-  &__radio-second:not(checked) + label:before {
-    top: 1rem;
-  }
-  &__radio:not(checked) + label:before {
-    content: '';
-    position: absolute;
+    top: 0;
     left: 0;
-    width: 1.5rem;
-    height: 1.5rem;
-    border: 0.1rem solid $grey !important;
-    border-radius: 0.3rem !important;
-    background: $grey !important;
-    transform: translateY(-50%);
+    width: 100%;
+    height: 100%;
+    line-height: 100%;
+    cursor: pointer;
+    user-select: none;
   }
-  &__radio-first:not(checked) + label:after {
-    top: 3.6rem;
-  }
-  &__radio-second:not(checked) + label:after {
-    top: 1rem;
-  }
-  &__radio:not(checked) + label:after {
-    content: '';
-    position: absolute;
-    left: 0.588rem;
-    width: 1rem !important;
-    height: 1rem !important;
-    border-radius: 0 !important;
-    background-image: url('@/assets/icons/checkbox.svg') !important;
-    background-repeat: no-repeat;
-    background-position: center;
-    opacity: 0;
-    transition: all 0.3s ease-in-out;
-    transform: translateY(-50%) !important;
-  }
-  &__radio:checked + label:after {
-    opacity: 1;
-  }
-  label {
-    font-family: 'Montserrat Regular';
-    @include adaptive-font(1.4, 1.2);
-    color: $dark-blue;
+  &-text {
+    font-size: 1.4rem;
+    line-height: 100%;
   }
   .error-message {
     display: block;
     font-family: 'Montserrat Regular';
     @include adaptive-font(1.2, 1);
     color: $dark-blue;
-  }
-}
-.chbx__checkbox {
-  .chbx__radio:not(checked) + label:before {
-    border: none;
-    background: $grey;
-    border-radius: 0.3rem;
-  }
-  .chbx__radio-first:not(checked) + label:after {
-    top: 3.6rem;
-  }
-  .chbx__radio-second:not(checked) + label:after {
-    top: 1rem;
-  }
-  .chbx__radio:not(checked) + label:after {
-    position: absolute;
-    left: 0.288rem;
-    border-radius: unset;
-    transform: translateY(-50%);
   }
 }
 </style>
